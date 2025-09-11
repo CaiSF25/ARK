72670aa81e31eec52ad033c238eb485b92ee5806
CaiSF25 <598987847@qq.com>
2025-09-11 23:18:42 +0800

    左右斜屋顶

---
+++ Source/ARK/BuildingSystem/RoofLeftMaster.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BuildableMaster.h"
+#include "RoofLeftMaster.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARK_API ARoofLeftMaster : public ABuildableMaster
+{
+	GENERATED_BODY()
+	
+};
